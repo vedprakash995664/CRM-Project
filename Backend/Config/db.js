db38efa4568c9dd7a64314827ea3a63d6707eb06
Ganeshkumar02 <princepalrp@gmail.com>
2024-12-24 12:17:46 +0530

    aa
    bbb

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -21,8 +21,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/CRM_Project', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+
     });
     console.log('MongoDB connected successfully!');
   } catch (error) {
