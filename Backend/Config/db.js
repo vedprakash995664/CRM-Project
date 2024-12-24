bdb5263ba30d0bdc3b397bb340c712b5e65455ae
Ved Prakash <vedprakash995664@gmail.com>
2024-12-24 12:48:54 +0530

    done

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,31 +1,15 @@
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const connectDB = async () => {
-//   try {
-//     // Removed deprecated options
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log('MongoDB connected');
-//   } catch (err) {
-//     console.error('Error connecting to MongoDB:', err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/CRM_Project', {
-
-    });
-    console.log('MongoDB connected successfully!');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+    // Removed deprecated options
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
     process.exit(1);
   }
 };
